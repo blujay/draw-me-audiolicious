3833b1fd715fb870860dc51de03a02765f2766ac
Maf'j Alvarez <1101918+blujay@users.noreply.github.com>
2021-07-22 15:48:43 +0100

    Tidy up of Repo and adding Holon collections

---
+++ Assets/_Scripts/Audio viz/musicBarScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicBarScript : MonoBehaviour {
+
+    public Color barColor;
+    public Renderer vizMeshRenderer;
+    public float minScale;
+    public float scaleMultiplier;
+    public int band;
+    public MusicVisualizer Visualizer;
+
+	// Use this for initialization
+	void Start () {
+        vizMeshRenderer.material.color = Visualizer.bands[band].barColor;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var scale = Visualizer.bands[band].bandBuffer * scaleMultiplier + minScale;
+        transform.localScale = new Vector3(transform.localScale.x, scale, transform.localScale.z);
+        //transform.localScale = new Vector3(transform.localScale.x, Visualizer.bands[band].bandBuffer * scaleMultiplier + minScale, transform.localScale.z);
+        vizMeshRenderer.material.color = Visualizer.bands[band].barColor;
+    }
+
+}
